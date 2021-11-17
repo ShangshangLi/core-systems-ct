69d84f30e3a2607ab3cb802ae43eeef33905f6a9
ShangshangLi <lss1111@outlook.com>
2021-11-16 19:21:41 -0500

    esc

--- city-as-system/scripts/main.js
+++ city-as-system/scripts/main.js
@@ -5,27 +5,33 @@
 // console.log(doorX);
 // door.style.opacity = '0'; 
 
-const esc = document.querySelector("#esc");
-const escCircle = document.querySelector("#esc-circle");
+// const esc = document.querySelector("#esc");
+// const escCircle = document.querySelector("#esc-circle");
 // const budget = document.querySelector('#budget');
 
-esc.addEventListener("mouseover", mouseOverESC);
-esc.addEventListener("mouseout", mouseOutESC);
-escCircle.addEventListener("mouseover", mouseOverESC);
-escCircle.addEventListener("mouseout", mouseOutESC);
+// esc.addEventListener("mouseover", mouseOverESC);
+// esc.addEventListener("mouseout", mouseOutESC);
+// escCircle.addEventListener("mouseover", mouseOverESC);
+// escCircle.addEventListener("mouseout", mouseOutESC);
 
-function mouseOverESC() {
-    escCircle.style.color="#58c1cc";
-    // esc.style.width="500px";
-    console.log("get");
-//   budget.style.top = event.clientY + 1 + 'px' ;
-//   budget.style.left = event.clientX + 1 + 'px';
-//   budget.classList.add('show');
-//   const budgetText = budget.querySelector('p');
-//   budgetText.innerText = "Budget:"+currentData;
-}
+// function mouseOverESC() {
+//     esc.style.fill = "#ff0000";
+//     escCircle.fill="black";
+//     // esc.style.width="500px";
+//     console.log("get");
+// //   budget.style.top = event.clientY + 1 + 'px' ;
+// //   budget.style.left = event.clientX + 1 + 'px';
+// //   budget.classList.add('show');
+// //   const budgetText = budget.querySelector('p');
+// //   budgetText.innerText = "Budget:"+currentData;
+// }
 
-function mouseOutESC() {
-    // budget.classList.remove('show');
+// function mouseOutESC() {
+//     // budget.classList.remove('show');
     
-}
+// }
+
+document.getElementById("esc-button").onclick = function(){
+    esc.style.fill='yellow';
+    console.log("get")
+  }
